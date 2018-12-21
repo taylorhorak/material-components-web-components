fe076b916b53384783ec8fdb873c808eb7582770
Tim Johnson <jhnsntmthy@gmail.com>
2018-12-21 11:28:56 -0500

    added some fixes to the rescoping and the demos

---
+++ demos/extra/drawer.js
@@ -0,0 +1,1 @@
+import {Drawer} from "@authentic/mwc-drawer";
--- scripts/rescope-material.sh
+++ scripts/rescope-material.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-find ./packages/*/package.json -exec sed -i '' -e 's/@authentic\/mwc/@material\/mwc/g' {} \;
+find ./demos/* -exec sed -i '' -e 's/@authentic\/mwc/@material\/mwc/g' {} \;
+find ./packages/* -exec sed -i '' -e 's/@authentic\/mwc/@material\/mwc/g' {} \;
 find ./packages/*/package.json -exec sed -i '' -e 's/AuthX\//material-components\//g' {} \;
