fcf163671062951cdedfd3c4cee91a53a65f7ab7
Luis Sardon <lmsardon@gmail.com>
2019-01-31 08:58:38 -0500

    version bump 0.3.13

--- packages/dialog/src/mwc-dialog.ts
+++ packages/dialog/src/mwc-dialog.ts
@@ -26,15 +26,15 @@ import {
     observer,
     queryAll,
     classMap
-} from "@material/mwc-base/base-element";
+} from "@authentic/mwc-base/base-element";
 import { closest, matches } from '@material/dom/ponyfill';
 import MDCDialogFoundation from "@material/dialog/foundation";
 import { strings } from "@material/dialog/constants";
 // Commented due to focus-trap not working
 // import * as createFocusTrap from 'focus-trap';
 // import * as util from '@material/dialog/util';
-import { emit } from '@material/mwc-base/utils';
-import { Button as MWCButton } from '@material/mwc-button';
+import { emit } from '@authentic/mwc-base/utils';
+import { Button as MWCButton } from '@authentic/mwc-button';
 
 import { style } from './mwc-dialog-css.js';
 import 'wicg-inert/dist/inert.js';
